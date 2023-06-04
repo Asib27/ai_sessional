b9dbf09a96490ac5209b479f5a461bcb1bc4dfca
asib27 <asib.rahman1927@gmail.com>
2023-06-05 00:08:03 +0600

    implement solvable function

--- offline1/main.cpp
+++ offline1/main.cpp
@@ -8,12 +8,13 @@ class Board
 private:
     vector<vector<int>> board;
 
-    vector<int> flat(){
+    vector<int> flatten(){
         vector<int> v;
 
         for(auto &i: board){
             for(auto j: i){
-                v.push_back(j);
+                if( j != 0)
+                    v.push_back(j);
             }
         }
 
@@ -23,6 +24,31 @@ private:
     int getGoalNumber(int row, int col, int size){
         if(row == size-1 && col == size-1) return 0;
         return row*size + col + 1;
+    }
+
+    int findInversionCount(){
+        int inversion = 0;
+        vector<int> flat = flatten();
+
+        for(int i = 0; i < flat.size(); i++){
+            for(int j = i+1; j < flat.size(); j++){
+                if(flat[i] > flat[j]) inversion++;
+            }
+        }
+
+        return inversion;
+    }
+
+    int getBlankSquareRow(){
+        int size = getSize();
+
+        for(int i = 0; i < size; i++){
+            for(auto j: board[i]){
+                if(j == 0) return i;
+            }
+        }
+
+        return -1;
     }
 
 public:
@@ -81,7 +107,15 @@ public:
     }
 
     bool isSolvable(){
-        return true;
+        int inversions = findInversionCount();
+        int size = getSize();
+
+        if(size%2 == 1){
+            return inversions%2 == 0;
+        }else{
+            int blankSquareRow = getBlankSquareRow();
+            return (inversions+blankSquareRow) % 2;
+        }
     }
 
     friend ostream& operator <<(ostream &os, Board &board){
@@ -110,8 +144,57 @@ void isGoalTest(){
     goal = {vector<int>{4,2,3}, vector<int>{5,1,6}, vector<int>{7,8,0}};
     board = Board(goal);
     if(board.isGoal() != 0) cout << board;
+
+    cout << "goal test done" << endl;
+}
+
+void isSolvableTest(){
+    vector<vector<int>> goal = {vector<int>{1,2,3}, vector<int>{4,5,6}, vector<int>{7,8,9}};
+    Board board(goal);
+    if(board.isSolvable() != 1) cout << board;
+
+    goal = {vector<int>{1,2,3}, vector<int>{4,5,6}, vector<int>{8,0,7}};
+    board = Board(goal);
+    if(board.isSolvable() != 0) cout << board;
+
+
+    goal = {vector<int>{1,2,3}, vector<int>{0,4,6}, vector<int>{8,5,7}};
+    board = Board(goal);
+    if(board.isSolvable() != 0) cout << board;
+
+
+    goal = {vector<int>{0,1,3}, vector<int>{4,2,5}, vector<int>{7,8,6}};
+    board = Board(goal);
+    if(board.isSolvable() != 1) cout << board;
+
+    cout << "solvable test done" << endl;
+}
+
+
+void is4SolvableTest(){
+    vector<vector<int>> goal = {vector<int>{1,2,3,4}, vector<int>{5,6,7,8}, vector<int>{9,10,11,12}, vector<int>{13,14,15,0}};
+    Board board(goal);
+    if(board.isSolvable() != 1) cout << board;
+
+    goal = {vector<int>{1,2,3,4}, vector<int>{5,6,7,8}, vector<int>{9,10,0,11}, vector<int>{13,14,15,12}};
+    board = Board(goal);
+    if(board.isSolvable() != 1) cout << board;
+
+
+    goal = {vector<int>{1,2,3,4}, vector<int>{5,6,0,8}, vector<int>{9,10,7,11}, vector<int>{13,14,15,12}};
+    board = Board(goal);
+    if(board.isSolvable() != 1) cout << board;
+
+
+    goal = {vector<int>{1,2,3,4}, vector<int>{5,6,7,8}, vector<int>{9,10,11,12}, vector<int>{13,15,14,0}};
+    board = Board(goal);
+    if(board.isSolvable() != 0) cout << board;
+
+    cout << "4 solvable test done" << endl;
 }
 
 int main(){
     isGoalTest();
+    isSolvableTest();
+    is4SolvableTest();
 }
